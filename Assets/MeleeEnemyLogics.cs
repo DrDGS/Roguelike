305afec1a8d470d923b569c9e54b4105ef3cae68
DrDGS <t.khafizov05@gmail.com>
2023-05-13 13:20:37 +0300

    MeleeEnemy player follow logics added

---
+++ Assets/MeleeEnemyLogics.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyLogics : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+    public float steering;
+
+    private Rigidbody2D rb;
+    private Transform tr;
+    private Vector2 moveVector;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveVector = Vector3.MoveTowards(tr.position, player.position, speed / 100) - tr.position;
+        moveVelocity = Vector3.Lerp(moveVelocity, moveVector.normalized * speed, steering * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
